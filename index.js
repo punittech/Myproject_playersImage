4e9119f55b7f3e7ebe017005733f2fdec8c09971
Puneet kumar mishra <punitms005@gmail.com>
2025-05-21 14:57:45 +0530

    task interview

---
+++ index.js
@@ -0,0 +1,20 @@
+    const tableContainer = document.querySelector('.table-container');
+    const table = document.getElementById('myTable');
+
+    tableContainer.addEventListener('mouseover', () => {
+      table.style.display = 'block';
+    });
+
+    tableContainer.addEventListener('mouseout', () => {
+      table.style.display = 'none';
+    });
+
+
+
+//   function mouseOver() {
+//   document.getElementById("myTable").style.display = "none";
+//   }
+
+// function mouseLeave() {
+//   document.getElementById("myTable").style.display = "block";
+// }
